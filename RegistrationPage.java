a2e846730532821db173297c43578167491193ba
Negoi Timotei <106741450+TIMOTEI16@users.noreply.github.com>
2022-06-16 13:42:04 +0300

    Add files via upload

---
+++ RegistrationPage.java
@@ -0,0 +1,109 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class RegistrationPage {
+
+    @FindBy(name = "Gender")
+    List<WebElement> genderRadioButtons;
+
+    @FindBy(id = "FirstName")
+    WebElement firstNameimput;
+
+    @FindBy(id = "LastName")
+    WebElement lastNameimput;
+    @FindBy(id = "Email")
+    WebElement emailimput;
+    @FindBy(id = "Company")
+    WebElement companyimput;
+    @FindBy(id = "Newsletter")
+    WebElement newslettercheckbox;
+    @FindBy(id = "Password")
+    WebElement passwordimput;
+    @FindBy(id = "ConfirmPassword")
+    WebElement confirmPassworfimput;
+    @FindBy(name = "DateOfBirthDay")
+    private WebElement dobDropdown;
+    @FindBy(name = "DateOfBirthMonth")
+    private WebElement mounthDobDropDown;
+    @FindBy(name = "DateOfBirthYear")
+    private WebElement yearDobDropDown;
+    @FindBy(xpath = "//*[@id=\"register-button\"]")
+    private WebElement registerbutton;
+    @FindBy(xpath = "//div[contains(text(),'Your registration completed')]")
+    private WebElement validMessage;
+
+    @FindBy(xpath = "//li[contains(text(),'The specified email already exists')]")
+    private WebElement errorMessage;
+
+    private WebDriver driver;
+
+    public RegistrationPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void selectGender(String gender) {
+
+        if (gender.equalsIgnoreCase("Male")) {
+            genderRadioButtons.get(0).click();
+        } else {
+            genderRadioButtons.get(1).click();
+        }
+    }
+
+
+    public void selectDayOfBirth(String day) {
+        Select select = new Select(dobDropdown);
+        select.selectByVisibleText(day);
+    }
+
+    public void selectDateOfBirthMonth(String month) {
+        Select select = new Select(mounthDobDropDown);
+        select.selectByVisibleText(month);
+    }
+
+    public void selectDateOfBirthYear(String year) {
+        Select select = new Select(yearDobDropDown);
+        select.selectByVisibleText(year);
+    }
+
+
+    public void selectNewsLetterCheckBox(boolean newsLetter) {
+        if (newsLetter == false)
+            newslettercheckbox.click();
+
+    }
+
+    public void registerUs(String gender, String firstName, String lastName, String day, String month, String year, String email, String company, boolean newsLetter, String password, String confirmPassword) {
+        selectGender(gender);
+        firstNameimput.sendKeys(firstName);
+        lastNameimput.sendKeys(lastName);
+        selectDayOfBirth(day);
+        selectDateOfBirthMonth(month);
+        selectDateOfBirthYear(year);
+        emailimput.sendKeys(email);
+        companyimput.sendKeys(company);
+        selectNewsLetterCheckBox(newsLetter);
+        passwordimput.sendKeys(password);
+        confirmPassworfimput.sendKeys(confirmPassword);
+        registerbutton.click();
+
+    }
+
+    public String getValidMessage() {
+        return validMessage.getText();
+    }
+
+    public String getErrorMessage() {
+        return errorMessage.getText();
+
+    }
+
+}
---
+++ RegistrationTest.java
@@ -0,0 +1,44 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.LoginPage;
+import pages.RegistrationPage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RegistrationTest {
+
+    RegistrationPage registrationPage;
+    WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\WebDrivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://demo.nopcommerce.com/register?returnUrl=%2F");
+        registrationPage = new RegistrationPage(driver);
+    }
+
+    @Test
+    public void canRegiterWithValidCredentials() {
+        registrationPage.registerUs("Male", "Timmy", "Test", "16", "May", "1999", "timmizc@test.com", "Endava", true, "test123", "test123");
+        assertEquals(registrationPage.getValidMessage(), "Your registration completed");
+    }
+
+
+    @Test
+    public void cannotRegiterWithValidCredentials() {
+        registrationPage.registerUs("Male", "Timmy", "Test", "16", "May", "1999", "timmyyy@test.com", "Endava", true, "test123", "test123");
+        assertEquals(registrationPage.getErrorMessage(), "The specified email already exists");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+}
+
